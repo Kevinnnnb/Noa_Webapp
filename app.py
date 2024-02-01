a4b0400ffd4ed8b2917233f5c569a42a62a40890
MakeItForLess <makeitforlessshop@gmail.com>
2024-01-31 20:18:26 -0700

    fix image name path

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def upload_file():
     <title>Upload new File</title>
     <h1>Upload new File</h1>
     <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
+      <input type=file name=image_data>
       <input type=submit value=Upload>
     </form>
     '''
