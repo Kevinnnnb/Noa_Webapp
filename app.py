12792e0aedf07e60e96610afb280ee73f723256a
MakeItForLess <makeitforlessshop@gmail.com>
2024-01-31 19:37:36 -0700

    added file delete function

--- app.py
+++ app.py
@@ -19,3 +19,9 @@ def load_test():
     with open('test.txt', 'w') as file:
         file.write("save test")
     return "wrote file"
+
+@app.route('/deleteFile')
+def load_test():
+    if os.path.exists("test.txt"):
+        os.remove("test.txt") # one file at a time
+    return "deleted"
