1d7ac10605d7b7a4e9e8441e09fadcf8eab4c466
MakeItForLess <makeitforlessshop@gmail.com>
2024-01-31 19:27:42 -0700

    long poll test

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+import os
+import time
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/longPoll')
+def long_poll():
+    while not(os.path.exits("test.txt")):
+        time.sleep(1)
+    testFile = open("test.txt","r")
+    return "done"
+
+@app.route('/loadTest')
+def load_test():
+    with open('test.txt', 'w') as file:
+        file.write("save test")
+    return "wrote file"
