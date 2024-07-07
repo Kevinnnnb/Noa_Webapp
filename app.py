5c11f19a6bd38da8ed5f4c5281ff813b3ec48568
Kevin <136887074+Kevinnnnb@users.noreply.github.com>
2024-07-07 19:52:09 +0200

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from flask import Flask, request, render_template, send_file, make_response
+from flask import Flask, request, render_template, send_file, make_response
 from werkzeug.utils import secure_filename
 import os
 
