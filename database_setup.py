a3da0518a7570c24c026c7096e35d61c890c1ff4
Kevin <136887074+Kevinnnnb@users.noreply.github.com>
2024-07-15 13:14:11 +0200

    Create database_setup.py

---
+++ database_setup.py
@@ -0,0 +1,13 @@
+from app import create_app, db
+from app.models import Stats
+
+app = create_app()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    db.create_all()
+    initial_stats = Stats(message_count=0, image_count=0)
+    db.session.add(initial_stats)
+    db.session.commit()
+    print('Initialized the database.')
