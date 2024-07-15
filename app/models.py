67afa31771de0ec3f8c4c4368df48f6f08c55b3d
Kevin <136887074+Kevinnnnb@users.noreply.github.com>
2024-07-15 13:20:17 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,19 @@
+from app import db
 
+class Stats(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message_count = db.Column(db.Integer, default=0)
+    image_count = db.Column(db.Integer, default=0)
+
+    def increment_message_count(self):
+        self.message_count += 1
+        db.session.commit()
+
+    def increment_image_count(self):
+        self.image_count += 1
+        db.session.commit()
+
+    def reset_stats(self):
+        self.message_count = 0
+        self.image_count = 0
+        db.session.commit()
