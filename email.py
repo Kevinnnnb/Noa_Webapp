a3d7ed24f7ce12af9bf739d5cf6d30a886a01f43
Kevin <136887074+Kevinnnnb@users.noreply.github.com>
2024-07-24 00:58:03 +0200

    Create email.py

---
+++ email.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_email(sender_email, sender_password, recipient_email, subject, body):
+    html_message = MIMEText(body, 'html')
+    html_message['Subject'] = subject
+    html_message['From'] = sender_email
+    html_message['To'] = recipient_email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, recipient_email, html_message.as_string())
+
+    print("Email envoyé !")
+
+# Exécution de la fonction
+
+sender_email = "yourname@mail.com"
+sender_password = "your security key" # For the password you need to go to your google account and search for app password -> you will have a 16 digits password for your script 
+
+recipient_email = "yourname@mail.com"
+subject = "Hello World !"
+body = """ Your content here ! """
+
+send_email(sender_email, sender_password, recipient_email, subject, body)
