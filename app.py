a7143208d0fccc5b04a7821b95379593f24a57f4
Kevin <136887074+Kevinnnnb@users.noreply.github.com>
2024-07-25 13:14:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -361,10 +361,14 @@ def send_email_password(sender_email, sender_password, recipient_email, subject_
     data = c.fetchall()
     conn.close()
     bite = data[0]
+
+    #test pour voir si la variable bite est un Tuple ou non
+    print(bite)
+    print(bite[0])
     
     body_password = body_password.replace('{{user}}', user)
     body_password = body_password.replace('{{password}}', password)
-    body_password = body_password.replace('{{token}}', bite)  # Remplacez {{token}} par {{cle}} dans le corps de l'email
+    body_password = body_password.replace('{{token}}', bite[0])  # Remplacez {{token}} par {{cle}} dans le corps de l'email
     html_message = MIMEText(body_password, 'html')
     html_message['Subject'] = subject_password
     html_message['From'] = sender_email
